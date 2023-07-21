30e6a0e2a9b6bfd7ab3dc8374e3cf5592532c738
saboumariusbogdan <100289634+GeluHorotan@users.noreply.github.com>
2023-07-22 01:14:43 +0300

    feat: refining the view styles

--- app/controllers/account.controller.js
+++ app/controllers/account.controller.js
@@ -1,10 +1,27 @@
 angular
   .module("app")
-  .controller("AccountController", function ($scope, AuthService, $location) {
-    $scope.user = AuthService.getUser();
+  .controller(
+    "AccountController",
+    function ($scope, AuthService, $location, $interval) {
+      $scope.user = AuthService.getUser();
 
-    $scope.logout = function () {
-      AuthService.logout();
-      $location.path("/login"); // Redirect to /login view after logout
-    };
-  });
+      $scope.logout = function () {
+        AuthService.logout();
+        $location.path("/login");
+      };
+
+      function updateDateTime() {
+        $scope.currentTime = {
+          date: new Date(),
+          time: new Date().toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+          }),
+        };
+      }
+
+      updateDateTime();
+      $interval(updateDateTime, 1000);
+    }
+  );
